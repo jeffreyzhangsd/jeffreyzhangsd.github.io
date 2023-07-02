494cca7c9476327890bd7d7f8a71722d1c77fcb7
Jeffrey Zhang <jzhang22la@gmail.com>
2023-07-01 18:49:40 -0700

    fixed up css, made base template and appropriate background, going to work on inputting image to animate

---
+++ src/components/Animate.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+
+const createAnimation = () => {
+	// set scene and camera position
+	const scene = new THREE.Scene();
+	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.set(0, 0, 15);
+	camera.lookAt(0, 0, 0);
+
+	const renderer = new THREE.WebGLRenderer();
+
+	let container = document.getElementById("canvas");
+	let rendered = true;
+	let w = container.offsetWidth;
+	let h = container.offsetHeight;
+	console.log(w, h);
+	renderer.setSize(w, h);
+
+	if (rendered === true) {
+		container.appendChild(renderer.domElement);
+		rendered = false;
+	}
+
+	const resizeCanvas = () => {
+		const canvas = renderer.domElement;
+		let container = document.getElementById("canvas");
+		const width = container.clientWidth;
+		const height = container.clientHeight;
+
+		if (canvas.width !== width || canvas.height !== height) {
+			renderer.setSize(width, height);
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+			console.log(canvas, width, height);
+		}
+	};
+
+	const geometry = new THREE.BoxGeometry(1, 1, 1); // vertices for cube
+	const points = [];
+	points.push(new THREE.Vector3(-10, 0, 0));
+	points.push(new THREE.Vector3(0, 10, 0));
+	points.push(new THREE.Vector3(10, 0, 0));
+	const lineGeo = new THREE.BufferGeometry().setFromPoints(points);
+
+	const material = new THREE.MeshBasicMaterial({
+		color: 0x00ff00,
+	}); // material to color cube
+	const lineMat = new THREE.LineBasicMaterial({
+		color: 0x0000ff,
+	});
+
+	// object, takes geometry and applies material
+	const cube = new THREE.Mesh(geometry, material);
+	const line = new THREE.Line(lineGeo, lineMat);
+
+	scene.add(cube);
+	scene.add(line);
+
+	function animate() {
+		resizeCanvas();
+
+		line.rotation.y += 0.01;
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		renderer.render(scene, camera);
+		requestAnimationFrame(animate); // better than setinterval
+	}
+
+	animate();
+};
+
+export default createAnimation;
