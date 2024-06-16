8b03c1cab89f73e4a7a17a43b350650967d2c77a
Jeffrey Zhang <109628105+JeffreyZhangsd@users.noreply.github.com>
2024-06-15 23:15:30 -0700

    removed threejs, going for a more minimalist approach

--- src/components/App.js
+++ src/components/App.js
@@ -1,83 +1,92 @@
-import React, { useState, Suspense } from "react";
-import createAnimation from "./Animate";
+import React, { useEffect, useState, Suspense } from "react";
 import Home from "./Home";
 import Projects from "./Projects";
 import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 
 function App() {
-	React.useEffect(() => {
-		createAnimation();
-	}, []);
+  // State to manage the theme
+  const [theme, setTheme] = useState("light");
 
-	// views to switch what is rendered
-	const [view, setView] = useState({ name: "Home", viewProps: {} });
+  // Toggle theme function
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
-	const changeView = (name, someProps = {}) => {
-		return (moreProps = {}) => {
-			console.log("Changing view to: " + name);
-			setView({ name, viewProps: { ...someProps, ...moreProps } });
-		};
-	};
+  // Apply the theme to the body class
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
-	const renderView = () => {
-		switch (view.name) {
-			case "Home":
-				return <Home changeView={(v) => changeView(v)} />;
-			case "Projects":
-				return <Projects changeView={(v) => changeView(v)} />;
-			case "AboutMe":
-				return <AboutMe changeView={(v) => changeView(v)} />;
-			case "Contact":
-				return <Contact changeView={(v) => changeView(v)} />;
-			default:
-				return <Home changeView={(v) => changeView(v)} />;
-		}
-	};
+  // views to switch what is rendered
+  const [view, setView] = useState({ name: "Home", viewProps: {} });
 
-	return (
-		<>
-			<div id="main">
-				<div className="container">
-					<div className="canvas-container" id="canvas">
-						<canvas className="canvas-container" id="webgl-canvas"></canvas>
-					</div>
-					<div className="header">
-						<h1>Jeffrey Zhang</h1>
-						<p>Full Stack Software Engineer</p>
-						<nav>
-							<ul className="tabs">
-								<li>
-									<a href="#home" onClick={changeView("Home")}>
-										Home
-									</a>
-								</li>
-								<li>
-									<a href="#projects" onClick={changeView("Projects")}>
-										Projects
-									</a>
-								</li>
-								<li>
-									<a href="#aboutme" onClick={changeView("AboutMe")}>
-										About Me
-									</a>
-								</li>
-								<li>
-									<a href="#contact" onClick={changeView("Contact")}>
-										Contact
-									</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
+  const changeView = (name, someProps = {}) => {
+    return (moreProps = {}) => {
+      console.log("Changing view to: " + name);
+      setView({ name, viewProps: { ...someProps, ...moreProps } });
+    };
+  };
 
-					<main>
-						<Suspense fallback={<p>Loading!</p>}>{renderView()}</Suspense>
-					</main>
-				</div>
-			</div>
-		</>
-	);
+  const renderView = () => {
+    switch (view.name) {
+      case "Home":
+        return <Home changeView={(v) => changeView(v)} />;
+      case "Projects":
+        return <Projects changeView={(v) => changeView(v)} />;
+      case "AboutMe":
+        return <AboutMe changeView={(v) => changeView(v)} />;
+      case "Contact":
+        return <Contact changeView={(v) => changeView(v)} />;
+      default:
+        return <Home changeView={(v) => changeView(v)} />;
+    }
+  };
+
+  return (
+    <>
+      <div id="main">
+        <div className="container">
+          <div className="header">
+            <h1 href="#home" onClick={changeView("Home")}>
+              Jeffrey Zhang
+            </h1>
+            <p>Full Stack Software Engineer</p>
+            <nav>
+              <ul className="tabs">
+                <li>
+                  <a href="#projects" onClick={changeView("Projects")}>
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="#aboutme" onClick={changeView("AboutMe")}>
+                    About Me
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact" onClick={changeView("Contact")}>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <button className="toggleTheme" onClick={toggleTheme}>
+              {theme === "light" ? (
+                <i className="fas fa-moon" style={{ color: "#000000" }}></i>
+              ) : (
+                <i className="fas fa-sun" style={{ color: "#ffffff" }}></i>
+              )}
+            </button>
+          </div>
+
+          <main>
+            <Suspense fallback={<p>Loading!</p>}>{renderView()}</Suspense>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Home = () => {
-	return (
-		<>
-			<div>
-				<h1> HOME</h1>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <h1> Some generic thing about me being a SWE</h1>
+        <h3> a picture? </h3>
+      </div>
+    </>
+  );
 };
 
 export default Home;
