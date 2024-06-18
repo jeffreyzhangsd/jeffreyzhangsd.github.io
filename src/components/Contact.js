3b2c52874cb3feafc020c6dfacaccf72fdf8988f
Jeffrey Zhang <109628105+JeffreyZhangsd@users.noreply.github.com>
2024-06-18 01:05:03 -0700

    added social media to contact section, planning to make some kind of form -> will do some research first

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SocialMedia from "./SocialMedia";
+import Button from "./Button";
 
 const Contact = () => {
   return (
@@ -6,6 +8,8 @@ const Contact = () => {
       <div className="container">
         <div className="leftCol">
           <h1>Contact</h1>
+          <br />
+          <SocialMedia />
         </div>
         <div className="rightCol">{/* form? */}</div>
       </div>
