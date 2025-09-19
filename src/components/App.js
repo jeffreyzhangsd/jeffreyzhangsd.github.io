8833bd2709fa27a22e0d21238dd2b066c0b23d8e
jeffreyzhangsd <jeffreyzhangsd@gmail.com>
2025-09-18 22:58:43 -0700

    update page, fix always showing scroll, debug tech grid, debug errors, remove console log, update resume and email, move light/dark, set body to be easier color

--- src/components/App.js
+++ src/components/App.js
@@ -23,12 +23,13 @@ function App() {
 
   const changeView = (name, someProps = {}) => {
     return (moreProps = {}) => {
-      console.log("Changing view to: " + name);
+      // console.log("Changing view to: " + name);
       setView({ name, viewProps: { ...someProps, ...moreProps } });
     };
   };
 
-  const resumeURL = "https://drive.google.com/file/d/1nOeUaGHnybyCiKU7N1UyW8IJ1LfOaKju/view?usp=sharing"
+  const resumeURL =
+    "https://docs.google.com/document/d/e/2PACX-1vTgJ7NWdytfXo9WOdfXCsIT2ESU5tsU7XLBDvdkHfvQKhUc1sgLiz8VmI2s4Fanfsud4U3lL0Ar4-dM/pub";
 
   const renderView = () => {
     switch (view.name) {
@@ -53,39 +54,43 @@ function App() {
     <>
       <div className="appContainer">
         <div className="header">
-          <a id="name" href="#home" onClick={changeView("Home")}>
-            Jeffrey Zhang
-          </a>
-          <p>
-            <br />
-            Full Stack Software Engineer
-          </p>
+          <div>
+            <a id="name" href="#home" onClick={changeView("Home")}>
+              Jeffrey Zhang
+            </a>
+            <p>
+              <br />
+              Full Stack Software Engineer
+            </p>
+          </div>
           <nav>
-            <ul className="tabs">
-              <li>
-                <a href="#aboutme" onClick={changeView("AboutMe")}>
-                  About Me
-                </a>
-              </li>
-              <li>
-                <a href="#projects" onClick={changeView("Projects")}>
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a href="#contact" onClick={changeView("Contact")}>
-                  Contact
-                </a>
-              </li>
-            </ul>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <button className="toggleTheme" onClick={toggleTheme}>
+                {theme === "light" ? (
+                  <i className="fas fa-moon" style={{ color: "#000000" }}></i>
+                ) : (
+                  <i className="fas fa-sun" style={{ color: "#ffffff" }}></i>
+                )}
+              </button>
+              <ul className="tabs">
+                <li>
+                  <a href="#aboutme" onClick={changeView("AboutMe")}>
+                    About Me
+                  </a>
+                </li>
+                <li>
+                  <a href="#projects" onClick={changeView("Projects")}>
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact" onClick={changeView("Contact")}>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
           </nav>
-          <button className="toggleTheme" onClick={toggleTheme}>
-            {theme === "light" ? (
-              <i className="fas fa-moon" style={{ color: "#000000" }}></i>
-            ) : (
-              <i className="fas fa-sun" style={{ color: "#ffffff" }}></i>
-            )}
-          </button>
         </div>
 
         <main>
--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,35 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
-const SocialMedia = (props) => {
-  const IconWrapper = styled.span`
-    i {
-      background-color: var(--text-color);
-    }
-    &:hover i {
-      background-color: var(--text-color);
-      transition: 0.3s ease-in;
-    }
-  `;
+const IconWrapper = styled.span`
+  i {
+    background-color: var(--text-color);
+  }
+  &:hover i {
+    background-color: var(--text-color);
+    transition: 0.3s ease-in;
+  }
+`;
 
+const SocialMedia = (props) => {
   const socialMediaLinks = [
     {
       name: "Github",
       link: "https://github.com/jeffreyzhangsd/",
-      fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-      backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+      fontawesomeicon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+      backgroundcolor: "#181717", // Reference https://simpleicons.org/?q=github
     },
     {
       name: "LinkedIn",
       link: "https://www.linkedin.com/in/jeffreyzhangsd/",
-      fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-      backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+      fontawesomeicon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+      backgroundcolor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
     },
     {
       name: "Gmail",
-      link: "mailto:jzhang22la@gmail.com",
-      fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-      backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+      link: "mailto:jeffreyzhangsd@gmail.com",
+      fontawesomeicon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+      backgroundcolor: "#D14836", // Reference https://simpleicons.org/?q=gmail
     },
   ];
 
@@ -46,7 +46,7 @@ const SocialMedia = (props) => {
             target="_blank"
             rel="noopener noreferrer">
             <IconWrapper {...media} {...props}>
-              <i className={`fab ${media.fontAwesomeIcon}`}></i>
+              <i className={`fab ${media.fontawesomeicon}`}></i>
             </IconWrapper>
             <p>{media.name}</p>
           </a>
